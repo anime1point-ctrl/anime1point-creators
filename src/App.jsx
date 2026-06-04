d4ded15c54ed20e8f90aee6d24a7676efe8bee9f
Anime1Point <shivendra.kupsa@gmail.com>
2026-06-04 14:39:00 +0530

    Sprint 2: Update App.jsx routing - add Creators, CategoryPage routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-────────────────import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
 import MainLayout from './components/layout/MainLayout'
 import AdminLayout from './components/layout/AdminLayout'
 import ProtectedAdminRoute from './components/layout/ProtectedAdminRoute'
@@ -6,9 +6,10 @@ import { AdminProvider } from './context/AdminContext'
 import { VideoModalProvider } from './context/VideoModalContext'
 import Home from './pages/Home'
 import About from './pages/About'
-import Directory from './pages/Directory'
+import Creators from './pages/Creators'
 import VideoDetail from './pages/VideoDetail'
 import CreatorProfile from './pages/CreatorProfile'
+import CategoryPage from './pages/CategoryPage'
 import AdminLogin from './pages/admin/AdminLogin'
 import AdminDashboard from './pages/admin/AdminDashboard'
 import AdminCreators from './pages/admin/AdminCreators'
@@ -17,33 +18,33 @@ import AdminCategories from './pages/admin/AdminCategories'
 import AdminHomeSections from './pages/admin/AdminHomeSections'
 
 export default function App() {
-    return (
-          <AdminProvider>
-                <VideoModalProvider>
-                        <HashRouter>
-                                  <Routes>
-                                              <Route element={<MainLayout />}>
-                                                            <Route index element={<Home />} />
-                                                            <Route path="about" element={<About />} />
-                                                            <Route path="directory" element={<Directory />} />
-                                                            <Route path="video/:id" element={<VideoDetail />} />
-                                                            <Route path="creator/:id" element={<CreatorProfile />} />
-                                              </Route>Route>
-                                              <Route path="admin" element={<AdminLogin />} />
-                                              <Route element={<ProtectedAdminRoute />}>
-                                                            <Route element={<AdminLayout />}>
-                                                                            <Route path="admin/dashboard" element={<AdminDashboard />} />
-                                                                            <Route path="admin/creators" element={<AdminCreators />} />
-                                                                            <Route path="admin/videos" element={<AdminVideos />} />
-                                                                            <Route path="admin/categories" element={<AdminCategories />} />
-                                                                            <Route path="admin/home-sections" element={<AdminHomeSections />} />
-                                                            </Route>Route>
-                                              </Route>Route>
-                                              <Route path="*" element={<Navigate to="/" replace />} />
-                                  </Routes>Routes>
-                        </HashRouter>HashRouter>
-                </VideoModalProvider>VideoModalProvider>
-          </AdminProvider>AdminProvider>
-        )
+  return (
+    <AdminProvider>
+      <VideoModalProvider>
+        <HashRouter>
+          <Routes>
+            <Route element={<MainLayout />}>
+              <Route index element={<Home />} />
+              <Route path="about" element={<About />} />
+              <Route path="creators" element={<Creators />} />
+              <Route path="video/:id" element={<VideoDetail />} />
+              <Route path="creator/:id" element={<CreatorProfile />} />
+              <Route path="category/:slug" element={<CategoryPage />} />
+            </Route>
+            <Route path="admin" element={<AdminLogin />} />
+            <Route element={<ProtectedAdminRoute />}>
+              <Route element={<AdminLayout />}>
+                <Route path="admin/dashboard" element={<AdminDashboard />} />
+                <Route path="admin/creators" element={<AdminCreators />} />
+                <Route path="admin/videos" element={<AdminVideos />} />
+                <Route path="admin/categories" element={<AdminCategories />} />
+                <Route path="admin/home-sections" element={<AdminHomeSections />} />
+              </Route>
+            </Route>
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </HashRouter>
+      </VideoModalProvider>
+    </AdminProvider>
+  )
 }
-</AdminProvider>
